f03449294d6121807f5d112b147d8ee889a71338
refactor-ring <refactor_ring0@proton.me>
2025-02-10 15:04:08 +0100

    Updated most dependencies to their current major versions

--- src/files.rs
+++ src/files.rs
@@ -1,3 +1,4 @@
+use qrcodegen::QrCode;
 use {
   crate::{common::*, file_stream::FileStream, vfs::Vfs},
   maud::html,
@@ -308,8 +309,33 @@ impl Files {
     Ok(
       Response::builder()
         .header(header::CONTENT_TYPE, "image/svg+xml")
-        .body(Body::from(qr_code.to_svg_string(4)))
+        .body(Body::from(Self::get_svg_string(&qr_code, 4)))
         .expect("All arguments to response builder are valid"),
     )
+  }
+
+  fn get_svg_string(qr: &QrCode, border: i32) -> String {
+    assert!(border >= 0, "Border must be non-negative");
+    let mut result = String::new();
+    result += "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    result += "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n";
+    let dimension = qr.size().checked_add(border.checked_mul(2).unwrap()).unwrap();
+    result += &format!(
+      "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" viewBox=\"0 0 {0} {0}\" stroke=\"none\">\n", dimension);
+    result += "\t<rect width=\"100%\" height=\"100%\" fill=\"#FFFFFF\"/>\n";
+    result += "\t<path d=\"";
+    for y in 0 .. qr.size() {
+      for x in 0 .. qr.size() {
+        if qr.get_module(x, y) {
+          if x != 0 || y != 0 {
+            result += " ";
+          }
+          result += &format!("M{},{}h1v1h-1z", x + border, y + border);
+        }
+      }
+    }
+    result += "\" fill=\"#000000\"/>\n";
+    result += "</svg>\n";
+    result
   }
 }
--- src/https_request_handler.rs
+++ src/https_request_handler.rs
@@ -73,7 +73,6 @@ impl HttpsRequestHandler {
       .state();
 
     let rustls_config = ServerConfig::builder()
-      .with_safe_defaults()
       .with_no_client_auth()
       .with_cert_resolver(state.resolver());
     let acceptor = state.acceptor();
